f68e74d0a8b986e8f947b25f969f4da794b057f8
WonJune Kang <devsigner9920@icloud.com>
2021-06-27 12:03:24 +0900

    Initial commit

---
+++ src/main/kotlin/com/xcelits/rea/ReArtifactApplication.kt
@@ -0,0 +1,11 @@
+package com.xcelits.rea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReArtifactApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReArtifactApplication>(*args)
+}
---
+++ src/test/kotlin/com/xcelits/rea/ReArtifactApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xcelits.rea
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReArtifactApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
